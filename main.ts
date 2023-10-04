c555ad79bea8e9ac27627f1490ca140599c4439c
Patrick Coxall <mr.coxall@mths.ca>
2023-10-04 19:26:34 -0400

    typescript code

--- main.ts
+++ main.ts
@@ -2,7 +2,31 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program shows an If Statement.
 */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber: number = 0
+
+randomNumber = -1
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function () {
+  randomNumber = randint(0, 1)
+  basic.clearScreen()
+
+  // if randomNumber was 0
+  if (randomNumber == 0) {
+    basic.showString("Y")
+  }
+
+  // if randomNumber was 1
+  if (randomNumber == 1) {
+    basic.showString("N")
+  }
+
+  // pasue and show you are ready again
+  basic.pause(1000)
+  basic.showIcon(IconNames.Happy)
+})
