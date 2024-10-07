936fd427944b4f09ca64360bf72d1ec8d148009f
Patrick Coxall <mr.coxall@mths.ca>
2024-10-07 14:47:37 -0400

    Update main.py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,29 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import random
 
 
-display.scroll("Hello, World!")
+# variables
+random_number = -1
+
+random_number = -1
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if accelerometer.was_gesture("shake"):
+        random_number = random.randint(0, 1)
+        display.clear()
+
+        # if random_number was 0
+        if random_number == 0:
+            display.scroll("Y")
+
+        # if random_number was 1
+        if random_number == 1:
+            display.scroll("N")
+
+        # pause and show you are ready again
+        sleep(1000)
+        display.show(Image.HAPPY)
